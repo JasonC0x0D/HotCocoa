3004305d7195bd53c8804e090543820ec5bb4b32
JasonC0x0D <jason.n.cowin@gmail.com>
2017-01-17 22:02:50 -0800

    Finished Logic for FSM
    TODO - Functions

--- Hot_Cocoa_1-MainFSM.ino
+++ Hot_Cocoa_1-MainFSM.ino
@@ -9,7 +9,7 @@
 #define fillingLeft 7
 #define fillingRight 8
 #define checkOrder 9
-#define orderReady 10
+#define orderFilled 10
 #define drinksTaken 11
 
 #define hotWaterOff 0
@@ -18,12 +18,18 @@
 
 #define hotWaterPotPin 7
 #define mixerPin 8
+#define leftValve 9
+#define rightValve 10
 
 // Global Variables
 int mainState = waitForOrder;
 int waterHeaterState = hotWaterOff; 
 
+// Global Variables - Order
+bool orderRec = false; // True when order placed via web interface
+int orderCup = 0; // The number of cups ordered
 
+// Global Variables - Finite State Machines
 int msCount = 0; // On MKR1000 int max is 2,147,483,647.  (NOT SO ON UNO and similar boards!)
 // 24 hrs in ms is 8.6 million, this is plenty big enough for this use. (Remove note later?)
 bool hotWaterWanted = false; //If hot water is wanted
@@ -32,18 +38,20 @@ float waterTemp = 0; // Temp of the water in degrees F
 bool pumpWater = false; // If water should be pumped
 bool waterPumped = false; // If water has been pumped
 int mixTime = 60; // The desired mix time in seconds
-bool cupLeftFull = false; // false = empty , true = full (TODO change diagram)
-bool cupRightFull = false; // false = empty , true = full (TODO change diagram)
-int fillTime = 10; // The time in seconds to dispense the cocoa (time valve is open)
-int orderCup = 0; // The number of cups ordered 
+bool cupLeftFull = false; // false = empty , true = full 
+bool cupRightFull = false; // false = empty , true = full 
+int fillTime = 10; // The time in seconds to dispense the cocoa (time valve is open) 
 bool orderReady = false; // If the order is ready for pickup
 bool buttonPress = false; // The button pressed to signal drinks taken (TODO change name)
 bool refillButton = false; // The button pressed to signal a refill is wanted
+int waitTime = 0; // ------------------------------- TODO ------------ need to address wait time
 
 void setup() {
 
   pinMode(hotWaterPotPin, OUTPUT);
   pinMode(mixerPin, OUTPUT);
+  pinMode(leftValve, OUTPUT);
+  pinMode(rightValve, OUTPUT);
 }
 
 void loop() {
@@ -52,40 +60,87 @@ void loop() {
   switch (mainState)
   {
     case waitForOrder:
-
+      if(orderRec == true){
+        mainState = waitToStart;
+        msCount = 0;
+      }
       break;
     case waitToStart:
-
+      if(msCount >= (waitTime * 1000)){  // TODO diagram typo msCounter -> msCount
+        mainState = waitForWater;
+        hotWaterWanted = true;
+      }
       break;
     case waitForWater:
-      
+      if(hotWaterReady == true){
+        mainState = waitForPump;
+        pumpWater = true;
+      }
       break;
     case waitForPump:
-
+      if(waterPumped == true){
+        mainState = addPowder;
+        digitalWrite(mixerPin, HIGH);
+        dispensePowder();
+      }
       break;
     case addPowder:
-
+      if(true){
+        mainState = mix;
+        msCount = 0;
+      }
       break;
     case mix:
-
+      if(msCount >= (mixTime * 1000)){ // (TODO *1000 on diagram)
+        mainState = dispense;
+        digitalWrite(mixerPin, LOW);
+      }
       break;
     case dispense:
-
+      if(cupLeftFull == false){
+        mainState == fillingLeft;
+        digitalWrite(leftValve, HIGH);
+        msCount = 0;                   // (TODO add msCount = 0)
+      } else if (cupRightFull == false) {
+        mainState == fillingRight;
+        digitalWrite(rightValve, HIGH);
+        msCount = 0;                   // (TODO add msCount = 0)
+      }
       break;
     case fillingLeft:
-
+      if(msCount >= fillTime){
+        mainState = checkOrder;
+        cupLeftFull = true;
+      }
       break;      
     case fillingRight:
-
+      if(msCount >= fillTime){
+        mainState = checkOrder;
+        cupRightFull = true;
+      }
       break;      
     case checkOrder:
-
+      if(orderCup == 1){
+        mainState = orderFilled;
+        orderReady = true;
+      } else if ( orderCup == 2 && cupRightFull == true){ // (TODO fix diagram cupRightFull!!!)
+        mainState = orderFilled;
+        orderReady = true;  
+      } else if ( orderCup == 2 && cupRightFull == false){
+        mainState = waitForWater;
+      }
       break;
-    case orderReady:
-
+    case orderFilled:
+      if(buttonPress == true){
+        mainState = drinksTaken;
+        clearOrder();
+      }
       break;  
     case drinksTaken:
-
+      if(refillButton == true){
+        mainState = waitForWater;
+        orderCup == 1; // (TODO Add to diagram)
+      }
       break;  
   }
 
